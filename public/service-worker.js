185dd0c29d48c22baa741af83bb41fb346d7ccca
Amelia Davis <ameliaaltman22@gmail.com>
2020-04-26 12:52:59 -0600

    added webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
-console.log("Hi from your service-worker.js file!")
+// console.log("Hi from your service-worker.js file!")
 
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/assets/css/style.css",
-    "/dist/bundle.js",
+    // "/dist/bundle.js",
   ];
   
   const STATIC_CACHE = "static-cache-v1";
@@ -18,3 +18,37 @@ const FILES_TO_CACHE = [
         .then(() => self.skipWaiting())
     );
   });
+
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+
+//==========================
+//Need to add a POST
+//==========================
+
+
+//   evt.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log('[ServiceWorker] Pre-caching offline page');
+//       return cache.addAll(FILES_TO_CACHE);
+//     })
+// );
