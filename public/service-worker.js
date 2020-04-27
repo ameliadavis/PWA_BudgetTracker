74192c847d1867922447e2d7a61eec80f23c0bf0
Amelia Davis <ameliaaltman22@gmail.com>
2020-04-26 19:55:40 -0600

    Removed Webpack & corrected serviceworker file, also added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,63 @@
-// console.log("Hi from your service-worker.js file!")
-
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/assets/css/style.css",
-    // "/dist/bundle.js",
+    "/styles.css"
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
   
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches
-        .open(STATIC_CACHE)
-        .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(() => self.skipWaiting())
+  // install
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
+    self.skipWaiting();
   });
-
-  self.addEventListener("activate", event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-    event.waitUntil(
-      caches
-        .keys()
-        .then(cacheNames => {
-          // return array of cache names that are old to delete
-          return cacheNames.filter(
-            cacheName => !currentCaches.includes(cacheName)
-          );
-        })
-        .then(cachesToDelete => {
-          return Promise.all(
-            cachesToDelete.map(cacheToDelete => {
-              return caches.delete(cacheToDelete);
+  
+  // activate
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
             })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-
-//==========================
-//Need to add a POST
-//==========================
-
-
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log('[ServiceWorker] Pre-caching offline page');
-//       return cache.addAll(FILES_TO_CACHE);
-//     })
-// );
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+  }});
+  
