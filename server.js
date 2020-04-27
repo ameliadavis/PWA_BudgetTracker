a6432425b708b5c26a74df49c236b12281842bc4
Amelia Davis <ameliaaltman22@gmail.com>
2020-04-26 21:42:56 -0600

    adding heroku deployment

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+//if deployed use the deployed database. Otherwise use the local mongo headlines databs
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/transaction"
+
+//connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
